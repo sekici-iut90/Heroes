996f4429f60d5b20cfa21f0d74e537c1c19cfbbd
sugdenaz <sugdenaz.ekici@edu.univ-fcomte.fr>
2025-04-06 13:54:11 +0200

    ..

--- src/services/orgs.service.js
+++ src/services/orgs.service.js
@@ -52,12 +52,7 @@ async function getOrganisationByIdService(id, secret) {
     return answer
 }
 
-/* NB sur le nommage:
-  Ajouter Service à la fin de chaque fonction permet ensuite de les identifier
-  facilement lorsqu'elles sont importées dans d'autres modules.
-  Cela évite également de donner le même nom à une fonction qui serait définit dans ces modules,
-  notamment dans le store.
- */
+
 export {
     getAllOrganisationsService,
     createOrganisationService,
--- src/services/team.service.js
+++ src/services/team.service.js
@@ -37,12 +37,6 @@ async function teamRemoveHeroesService(data) {
     return answer
 }
 
-/* NB sur le nommage:
-  Ajouter Service à la fin de chaque fonction permet ensuite de les identifier
-  facilement lorsqu'elles sont importées dans d'autres modules.
-  Cela évite également de donner le même nom à une fonction qui serait définit dans ces modules,
-  notamment dans le store.
- */
 export {
     getAllTeamsService,
     createTeamService,
